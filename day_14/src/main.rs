4b55007056805d47da1d6520fb799081a3fc49a7
agirard <augirard17@gmail.com>
2024-01-21 19:03:40 +0100

    wip: part2 utilisation memoize

--- day_14/src/main.rs
+++ day_14/src/main.rs
@@ -1,23 +1,31 @@
+use process::calcul_total_load;
 use reader::read_lines;
 use transformer::transforme_entries;
 
-use crate::process::tilt_north;
+use crate::process::cycle;
 
 mod reader;
 mod transformer;
 mod process;
 
 fn main() {
-    let input_path = String::from("./input/data.txt");
+    let input_path = String::from("./input/calibration.txt");
 
     let mut map: Vec<Vec<char>> = Vec::new();
+
+    let nb_cycles: usize = 1000000000usize;
     
 
     if let Ok(lines) = read_lines(input_path) {
         transforme_entries(lines,&mut map);
 
         // calcul move position
-        let count = tilt_north(&mut map);
+        for i in 0..nb_cycles {
+            println!("boucle {:?}",i);
+            cycle(&mut map);
+        }
+
+        let count = calcul_total_load(&map);
 
         println!("{:?}",count);
     }
--- day_14/src/process.rs
+++ day_14/src/process.rs
@@ -1,8 +1,19 @@
-
+use memoize::memoize;
 
 pub fn cycle(map: &mut Vec<Vec<char>>) {
 
     tilt_north(map);
+    //display_map(map);
+    
+    tilt_west(map);
+  //  display_map(map);
+
+    tilt_south(map);
+  //  display_map(map);
+
+    tilt_east(map);
+  //  display_map(map);
+
 }
 
 
@@ -52,10 +63,139 @@ pub fn tilt_north(map: &mut Vec<Vec<char>>) -> usize {
     count
 }
 
-pub fn tilt_west(map: &mut Vec<Vec<char>>) {
 
+pub fn tilt_west(map: &mut Vec<Vec<char>>) {
+    let mut num_map: Vec<Vec<u16>> = Vec::new();
+    let map_tmp = map.clone();
+    
+    for (i,line) in map_tmp.iter().enumerate() {
+
+        let mut num_line: Vec<u16> = Vec::new();
+        for (j, caractere) in line.iter().enumerate() {
+            
+            match caractere {
+                '.' => {
+                    // incremente de 1 la valeur sur la meme colonne de la ligne precedente
+                    let mut prev_value = 0;
+                    if  j > 0{
+                        prev_value = num_line.get(j - 1).unwrap().clone();
+                    } 
+                    num_line.push(prev_value + 1u16);
+                },
+                '#' => {
+                    num_line.push(0u16);
+                },
+                'O' => {
+                    let mut prev_value = 0;
+                    if j > 0 {
+                        prev_value = num_line.get(j - 1).unwrap().clone();
+                    } 
+
+                    move_rounded_rock(map, (i,j), (i,j - prev_value as usize));
+                    //calcul_amount_of_load(i-prev_value  as usize, &mut count, nb_line);
+
+                    num_line.push(prev_value.clone());
+                },
+                _ => {
+                    panic!("unknow caractere");
+                }
+            }
+        }
+        num_map.push(num_line);
+    } 
 }
 
+
+pub fn tilt_south(map: &mut Vec<Vec<char>>) {
+    let mut num_map: Vec<Vec<u16>> = Vec::new();
+    let map_tmp = map.clone();
+    let nb_lines = map.len();
+    
+    // on inverse le sens
+    for (i,line) in map_tmp.iter().rev().enumerate() {
+
+        let real_i = nb_lines - 1 - i; // real index
+        
+        let mut num_line: Vec<u16> = Vec::new();
+        for (j, caractere) in line.iter().enumerate() {
+            
+            match caractere {
+                '.' => {
+                    // incremente de 1 la valeur sur la meme colonne de la ligne precedente
+                    let mut prev_value = 0;
+                    if i > 0 {
+                        prev_value = num_map.get(i - 1).unwrap().get(j).unwrap().clone();
+                    } 
+                    num_line.push(prev_value + 1u16);
+                },
+                '#' => {
+                    num_line.push(0u16);
+                },
+                'O' => {
+                    let mut prev_value = 0;
+                    if i > 0 {
+                        prev_value = num_map.get(i - 1).unwrap().get(j).unwrap().clone();
+                    } 
+
+                    move_rounded_rock(map, (real_i,j), (real_i + prev_value as usize,j));
+                    //calcul_amount_of_load(i-prev_value  as usize, &mut count, nb_line);
+
+                    num_line.push(prev_value.clone());
+                },
+                _ => {
+                    panic!("unknow caractere");
+                }
+            }
+        }
+        num_map.push(num_line);
+    }
+}
+
+
+pub fn tilt_east(map: &mut Vec<Vec<char>>) {
+    let mut num_map: Vec<Vec<u16>> = Vec::new();
+    let map_tmp = map.clone();
+    
+    for (i,line) in map_tmp.iter().enumerate() {
+
+        let mut num_line: Vec<u16> = Vec::new();
+        let len_line = line.len();
+        // on inverse
+        for (j, caractere) in line.iter().rev().enumerate() {
+
+            let real_j = len_line - 1 - j;
+            
+            match caractere {
+                '.' => {
+                    // incremente de 1 la valeur sur la meme colonne de la ligne precedente
+                    let mut prev_value = 0;
+                    if  j > 0{
+                        prev_value = num_line.get(j - 1).unwrap().clone();
+                    } 
+                    num_line.push(prev_value + 1u16);
+                },
+                '#' => {
+                    num_line.push(0u16);
+                },
+                'O' => {
+                    let mut prev_value = 0;
+                    if j > 0 {
+                        prev_value = num_line.get(j - 1).unwrap().clone();
+                    } 
+
+                    move_rounded_rock(map, (i,real_j), (i,real_j + prev_value as usize));
+                    //calcul_amount_of_load(i-prev_value  as usize, &mut count, nb_line);
+
+                    num_line.push(prev_value.clone());
+                },
+                _ => {
+                    panic!("unknow caractere");
+                }
+            }
+        }
+        num_map.push(num_line);
+    } 
+}
 
 pub fn move_rounded_rock(map: &mut Vec<Vec<char>>,origin: (usize,usize), destination: (usize,usize)) {
 
@@ -66,6 +206,29 @@ pub fn move_rounded_rock(map: &mut Vec<Vec<char>>,origin: (usize,usize), destina
     *map.get_mut(line_dest).unwrap().get_mut(col_dest).unwrap() = 'O';
 }
 
+
+pub fn calcul_total_load(map: &Vec<Vec<char>>) -> usize {
+    let mut count = 0usize;
+    let nb_line = map.len();
+    for (i,line) in map.iter().enumerate() {
+        for (_,caractere) in line.iter().enumerate() {
+
+            if *caractere == 'O' {
+                calcul_amount_of_load(i, &mut count, nb_line);
+            }
+        }
+    }
+    count
+}
+
 pub fn calcul_amount_of_load(line: usize,count: &mut usize, nb_line: usize) {
     *count += nb_line - line
+}
+
+pub fn display_map(map: &Vec<Vec<char>>) {
+    for line in map  {
+        println!("___________");
+        println!("{:?}",line);
+        println!("___________");
+    }
 }
