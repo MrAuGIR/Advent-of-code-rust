58e9c43261fe4931a8277d3f4159eefa21565474
agirard <augirard17@gmail.com>
2024-02-15 00:01:01 +0100

    component bloc

--- day_17/src/component.rs
+++ day_17/src/component.rs
@@ -5,3 +5,11 @@ pub enum Direction {
     Up,
     Down
 }
+
+#[derive(Debug,Clone,Eq, PartialEq, Hash)]
+pub struct Bloc {
+    pub x: usize,
+    pub y: usize,
+    pub lost: u32,
+    pub direction: Direction
+}
--- day_17/src/main.rs
+++ day_17/src/main.rs
@@ -1,9 +1,13 @@
 use reader::read_lines;
 use transformer::get_map;
 
+use crate::component::{Bloc, Direction};
+
 
 mod reader;
 mod transformer;
+mod component;
+mod process;
 
 fn main() {
     let input_path = "./input/calibration.txt";
@@ -11,6 +15,8 @@ fn main() {
     let content = read_lines(input_path);
 
     let map  = get_map(content.as_str());
+
+    let start = Bloc {x:0, y: 0, lost: map.get(0, 0),direction: Direction::Left};
 
     println!("{:?}",map);
 }
