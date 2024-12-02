266fcbde99a5a2c5bf04efa18b0fa7eaa7d82af5
agirard <augirard17@gmail.com>
2024-12-02 23:41:05 +0100

    resolve part 2 day 1

--- 2024/day_1/src/main.rs
+++ 2024/day_1/src/main.rs
@@ -2,6 +2,8 @@ mod reader;
 mod transformer;
 mod component;
 
+use std::collections::HashMap;
+
 use transformer::transform_line_to_lists;
 use component::List;
 
@@ -15,6 +17,8 @@ fn main() {
 
     let mut result: u64 = 0;
 
+    let mut occurances: HashMap<u64,u64> = HashMap::new();
+
     let mut first_list = listes.get(0).unwrap().clone();
     first_list.sort_numbers();
     let mut second_list = listes.get(1).unwrap().clone();
@@ -25,12 +29,26 @@ fn main() {
 
     for (index,number) in numbers_1.iter().enumerate() {
 
-        let second_number = numbers_2.get(index).unwrap();
-        let mut dif = number.max(second_number) - second_number.min(number);
-        
-        println!("index: {} -> substract {} - {}  = {} ",index,number, second_number, dif);
-        result += dif;
-        
+        get_occurences(index as u64,number, numbers_2, &mut occurances);
     }
+
+    for (key, value) in occurances.into_iter() {
+         
+        println!("{} = {} ",key,value);
+        result += value;
+    }
+
     println!("List {}",result);
+}
+
+fn get_occurences(search_index: u64,search: &u64, list: &Vec<u64>, occurances: &mut HashMap<u64,u64>)
+{   
+    let mut count: u64 = 0;
+    for (index,number) in list.iter().enumerate() {
+
+        if (search.eq(number)) {
+            count = count + 1;
+        }
+    }
+    occurances.insert(search_index.clone(), count * search);
 }
